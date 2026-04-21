65d39ff01d69a638b3d061fd0f0f86f7eb37e703
Mr.Indoram <arjunkumar73384@gmail.com>
2026-04-21 13:02:01 +0530

    Add files via upload

---
+++ js/scrpit.js
@@ -0,0 +1,14 @@
+
+// function checkScroll() {
+//     let navbar = document.getElementById("navbar-section");
+//     let arjun = window.scrollY;
+
+//     console.log(arjun);
+
+//     if (window.scrollY > 100) {
+//         // navbar.classList.replace("bg-transparent", "bg-dark");
+//     } else {
+//         // navbar.classList.replace("bg-dark", "bg-light");
+//     }
+// }
+// window.addEventListener("scroll", checkScroll);
